b619bebedad11474a514a9d047a7a234899807f6
clfe1001 <clfe1001@stud.hs-kl.de>
2022-09-24 14:20:20 +0200

    Triangle Implementierung und Test Klasse von Claudi Fetzer

--- src/main/java/triangle/TriangleClfe.java
+++ src/main/java/triangle/TriangleClfe.java
@@ -15,7 +15,7 @@ public class TriangleClfe implements Triangle
         } else if (lengths[0] <= 0 || lengths[1] <= 0 || lengths[2] <= 0)
         {
             return TriangleResult.UNKNOWN;
-        } else if (lengths[0] != lengths[1] && lengths[1] != lengths[2])
+        } else if (lengths[0] != lengths[1] && lengths[1] != lengths[2]&& lengths[0]!=lengths[2])
         {
             return TriangleResult.TRIANGLE_NOT_EQUAL;
         } else if ((lengths[0] == lengths[1] && lengths[1] != lengths[2]) || (lengths[1] == lengths[2] && lengths[0] != lengths[1]) || (lengths[0] == lengths[2] && lengths[1] != lengths[2]))
